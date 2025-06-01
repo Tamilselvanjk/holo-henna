7ed6bd8b0606ef15bfc25f1cd7e1ed2388f62073
Tamiljk <tamilselvanjegan408@gmail.com>
2025-06-01 11:53:29 +0530

    Update

--- frontend/backend/routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getProducts, getSingleProducts } = require('../controllers/productController');
-const router = express.Router();
-
-router.route('/products').get(getProducts);
-router.route('/products/:id').get(getSingleProducts);
-
-module.exports = router;
